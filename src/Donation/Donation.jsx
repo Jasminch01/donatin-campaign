a3d1d0b8b1f729807d93ca6b1dc58befce3033a1
jasmin <jasminchakma895@gmail.com>
2023-09-25 13:48:47 +0600

    donation page done

--- src/Donation/Donation.jsx
+++ src/Donation/Donation.jsx
@@ -1,8 +1,22 @@
+import DonationCard from "../DonationCard/DonationCard";
 
 const Donation = () => {
+
+    const donations = JSON.parse(localStorage.getItem('donations'));
+
+    console.log(donations)
+
+
     return (
         <div>
-           <h1>Donation</h1>
+            <div className="grid lg:grid-cols-2 w-[80%] mx-auto gap-5 my-10">
+                {
+                    donations.map(card => <DonationCard key={card.id} card = {card}></DonationCard> )
+                }
+            </div>
+            <div className="flex items-center justify-center my-5">
+                <button className="p-3 bg-green-600 text-white rounded">See all</button>
+            </div>
         </div>
     );
 };
---
+++ src/DonationCard/DonationCard.jsx
@@ -0,0 +1,41 @@
+import PropTypes from "prop-types";
+
+const DonationCard = ({ card }) => {
+  const { picture, title, price, category_bg_color, text_button_bg_color, category } =
+    card;
+
+  const cardStyle = {
+    backgroundColor: category_bg_color,
+  };
+  const textStyle = {
+    color: text_button_bg_color[0],
+  };
+  const btnStyle = {
+    color: text_button_bg_color[0],
+    backgroundColor: text_button_bg_color[1],
+  };
+  const viewBtn = {
+    backgroundColor: text_button_bg_color[0],
+  };
+
+  return (
+    <div>
+      <div style={cardStyle} className="p-5 flex gap-4 items-center rounded">
+        <div>
+          <img src={card.picture} alt={title} className="rounded" />
+        </div>
+        <div className="space-y-3">
+        <button style={btnStyle} className="p-2 rounded">{category}</button>
+            <p className="font-semibold text-2xl">{title}</p>
+            <p style={textStyle} className="text-base font-bold">$ {price}</p>
+            <button style={viewBtn} className="text-white p-3 rounded font-bold">View Details</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+DonationCard.propTypes = {
+  card: PropTypes.object.isRequired,
+};
+export default DonationCard;
