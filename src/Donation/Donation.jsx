bf5ecbdf122214b9acae83918909b12278bb8d38
jasmin <jasminchakma895@gmail.com>
2023-09-25 14:12:15 +0600

    see all button functionallity done

--- src/Donation/Donation.jsx
+++ src/Donation/Donation.jsx
@@ -1,24 +1,49 @@
+import { useEffect, useState } from "react";
 import DonationCard from "../DonationCard/DonationCard";
 
 const Donation = () => {
+  const [donations, setDonations] = useState([]);
+  const [noDataFound, setNoDataFound] = useState("");
+  const [isSeeAll, setIsSeeAll] = useState(false);
+  console.log(isSeeAll);
 
-    const donations = JSON.parse(localStorage.getItem('donations'));
-
-    console.log(donations)
-
-
-    return (
-        <div>
-            <div className="grid lg:grid-cols-2 w-[80%] mx-auto gap-5 my-10">
-                {
-                    donations.map(card => <DonationCard key={card.id} card = {card}></DonationCard> )
-                }
-            </div>
-            <div className="flex items-center justify-center my-5">
-                <button className="p-3 bg-green-600 text-white rounded">See all</button>
-            </div>
+  useEffect(() => {
+    const donations = JSON.parse(localStorage.getItem("donations"));
+    if (donations) {
+      setDonations(donations);
+    } else {
+      setNoDataFound("No Data Found");
+    }
+  }, []);
+  return (
+    <div>
+      <div className="grid lg:grid-cols-2 w-[80%] mx-auto gap-5 my-10">
+        {isSeeAll
+          ? donations.map((card) => (
+              <DonationCard key={card.id} card={card}></DonationCard>
+            ))
+          : donations
+              .slice(0, 4)
+              .map((card) => (
+                <DonationCard key={card.id} card={card}></DonationCard>
+              ))}
+      </div>
+      {donations.length > 4 && (
+        <div
+          className={
+            isSeeAll ? "hidden" : "flex items-center justify-center my-5"
+          }
+        >
+          <button
+            onClick={() => setIsSeeAll(!isSeeAll)}
+            className="p-3 bg-green-600 text-white rounded"
+          >
+            See all
+          </button>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Donation;
